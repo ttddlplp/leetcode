8f8a970f9ab1c2160f55c630dcdaf129aad469a6
Gaoxiang Zeng <ttddlplp@gmail.com>
2015-11-23 22:08:29 +0000

    add clone graph

--- src/main/java/UndirectedGraphNode.java
+++ src/main/java/UndirectedGraphNode.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import static java.util.stream.Collectors.toList;
+
 public class UndirectedGraphNode {
     int label;
     List<UndirectedGraphNode> neighbors;
@@ -14,17 +16,24 @@ public class UndirectedGraphNode {
             return null;
         }
 
-        int rootVal = Integer.valueOf(nodes[0].substring(0, 1));
+        int rootVal = Integer.valueOf(nodes[0].split(",")[0]);
         Map<Integer, UndirectedGraphNode> map = new HashMap<>();
         for (String node : nodes) {
-            char[] vals = node.toCharArray();
-            UndirectedGraphNode nodeObj = new UndirectedGraphNode(Integer.valueOf(vals[0]));
-            for (int i = 0; i < vals.length; i++) {
-
+            String[] vals = node.split(",");
+            UndirectedGraphNode nodeObj = getAndCreateIfNotExists(vals[0], map);
+            for (int i = 1; i < vals.length; i++) {
+                nodeObj.neighbors.add(getAndCreateIfNotExists(vals[i], map));
             }
         }
-        
+        return map.get(rootVal);
+    }
 
+    private static UndirectedGraphNode getAndCreateIfNotExists(String val, Map<Integer, UndirectedGraphNode> map) {
+        Integer key = Integer.parseInt(String.valueOf(val));
+        if (!map.containsKey(key)) {
+            map.put(key, new UndirectedGraphNode(key));
+        }
+        return map.get(key);
     }
 
     @Override
@@ -33,11 +42,39 @@ public class UndirectedGraphNode {
         if (o == null || getClass() != o.getClass()) { return false; }
         UndirectedGraphNode that = (UndirectedGraphNode) o;
         return Objects.equals(label, that.label) &&
-                Objects.equals(neighbors, that.neighbors);
+                Objects.equals(neighbors.size(), that.neighbors.size()) &&
+                Objects.equals(getNeighborsWithSelf(this, neighbors), getNeighborsWithSelf(that, neighbors));
+    }
+
+    private List<UndirectedGraphNode> getNeighborsWithSelf(UndirectedGraphNode undirectedGraphNode,
+                                                           List<UndirectedGraphNode> list) {
+        return
+                list.stream().filter(undirectedGraphNode1 ->  undirectedGraphNode != undirectedGraphNode)
+                        .collect(toList());
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(label, neighbors);
+    }
+
+    @Override
+    public String toString() {
+        return "UndirectedGraphNode{" +
+                "label=" + label +
+                ", neighbors=" + getNeighbourString() +
+                '}';
+    }
+
+    private String getNeighbourString() {
+        StringBuilder sb = new StringBuilder();
+        for (UndirectedGraphNode neighbor : neighbors) {
+            if (neighbor == this) {
+                sb.append("UndirectedGraphNode{label=" + neighbor.label+"}");
+            } else {
+                sb.append(neighbor.toString());
+            }
+        }
+        return sb.toString();
     }
 }
