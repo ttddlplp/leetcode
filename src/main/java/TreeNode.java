d7d143961c0d0aa2801f9bf6eee4c66dce38cd1f
Gaoxiang Zeng <ttddlplp@gmail.com>
2015-11-29 16:05:41 +0000

    Binary Inorder Traversal

--- src/main/java/TreeNode.java
+++ src/main/java/TreeNode.java
@@ -1,3 +1,6 @@
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * Created by gaoxiangzeng-personal on 15/11/25.
  */
@@ -6,5 +9,33 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
+
+    public static TreeNode fromString(String s) {
+        String[] split = s.split(",");
+
+        TreeNode root = new TreeNode(Integer.valueOf(split[0]));
+        int i = 1;
+        Queue<TreeNode> visit = new LinkedBlockingQueue<>();
+        visit.add(root);
+        while (i < split.length) {
+            TreeNode next = visit.poll();
+            String nextVal = split[i++];
+            if (!nextVal.equals("#")) {
+                next.left = new TreeNode(Integer.valueOf(nextVal));
+                visit.add(next.left);
+            }
+
+            if (i >= split.length) {
+                break;
+            }
+            nextVal = split[i++];
+            if (!nextVal.equals("#")) {
+                next.right = new TreeNode(Integer.valueOf(nextVal));
+                visit.add(next.right);
+            }
+        }
+
+        return root;
+    }
 }
 
