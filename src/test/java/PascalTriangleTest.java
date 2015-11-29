3057dc0c1a6957c51c0980d2e6d0775abdec124b
Gaoxiang Zeng <ttddlplp@gmail.com>
2015-11-29 12:00:21 +0000

    Pascal triangle

---
+++ src/test/java/PascalTriangleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by gaoxiangzeng-personal on 15/11/29.
+ */
+public class PascalTriangleTest {
+    @Test
+    public void testName() throws Exception {
+        PascalTriangle pascalTriangle = new PascalTriangle();
+        System.out.println(pascalTriangle.generate(5));
+
+    }
+}
