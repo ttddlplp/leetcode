9d252369647f6b5fef676292efc02869cc0d27f4
Gaoxiang Zeng <ttddlplp@gmail.com>
2015-12-01 21:31:46 +0000

    maximal rec unsolved

---
+++ src/main/java/MaximalRectangle.java
@@ -0,0 +1,19 @@
+/**
+ * Created by gaoxiangzeng-personal on 15/12/1.
+ */
+public class MaximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length < 1 || matrix[0] == null || matrix[0].length < 1) {
+            return 0;
+        }
+
+        int dp[][] = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+            }
+        }
+        return 0;
+    }
+}
