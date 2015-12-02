52494dae21085d9e2c6fa0f985acd7816f266f57
gaoxiang.zeng <ttddlplp@gmail.com>
2015-12-02 17:40:11 +0000

    climbingStair

---
+++ src/main/java/ClimbingStair.java
@@ -0,0 +1,17 @@
+public class ClimbingStair {
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n <= 2) {
+            return n;
+        }
+        
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n - 1];
+    }
+}
---
+++ src/test/java/ClimbingStairTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClimbingStairTest {
+
+    private ClimbingStair climbingStair;
+
+    @Before
+    public void setUp() throws Exception {
+        climbingStair = new ClimbingStair();
+    }
+
+    @Test
+    public void test1() throws Exception {
+        assertEquals(1, climbingStair.climbStairs(1));
+    }
+    
+    @Test
+    public void test2() throws Exception {
+        assertEquals(2, climbingStair.climbStairs(2));
+    }
+    
+    @Test
+    public void test3() throws Exception {
+        assertEquals(3, climbingStair.climbStairs(3));
+    }
+}
