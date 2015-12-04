3fa6a720fafad034c434d19cce1f815d86c2311a
Gaoxiang Zeng <ttddlplp@gmail.com>
2015-12-04 21:47:53 +0000

    LinkedListCycle

---
+++ src/main/java/LinkedListCycle.java
@@ -0,0 +1,23 @@
+/**
+ * Created by gaoxiangzeng-personal on 15/12/4.
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode step1 = head;
+        ListNode step2 = head;
+
+        while (step2.next != null && step2.next.next != null) {
+            step1 = step1.next;
+            step2 = step2.next.next;
+
+            if (step1 == step2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
