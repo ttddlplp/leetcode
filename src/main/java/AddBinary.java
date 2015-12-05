49f8c3b224ca87d4fa8d0f66c8c6cf4a92690604
Gaoxiang Zeng <ttddlplp@gmail.com>
2015-12-05 22:34:46 +0000

    add binary improve

--- src/main/java/AddBinary.java
+++ src/main/java/AddBinary.java
@@ -7,61 +7,21 @@ public class AddBinary {
             return "";
         }
 
-        int inverseIndex = 1;
         StringBuilder sb = new StringBuilder(Math.max(a.length(), b.length()) + 1);
 
-        boolean addOne = false;
-        while (a.length() - inverseIndex >= 0 || b.length() - inverseIndex >= 0) {
-            int indexA = a.length() - inverseIndex;
-            int indexB = b.length() - inverseIndex;
-            if (getaChar(a, indexA) == '1') {
-                if (getaChar(a, indexA) == getaChar(b, indexB)) {
-                    if (!addOne) {
-                        sb.append('0');
-                    } else {
-                        sb.append('1');
-                    }
-                    addOne = true;
-                } else {
-                    if (addOne) {
-                        sb.append('0');
-                        addOne = true;
-                    } else {
-                        sb.append('1');
-                        addOne = false;
-                    }
-                }
-            } else {
-                if (getaChar(a, indexA) == getaChar(b, indexB)) {
-                    if (!addOne) {
-                        sb.append('0');
-                    } else {
-                        sb.append('1');
-                    }
-                    addOne = false;
-                } else {
-                    if (addOne) {
-                        sb.append('0');
-                        addOne = true;
-                    } else {
-                        sb.append('1');
-                        addOne = false;
-                    }
-                }
-            }
-            inverseIndex++;
+        int carry = 0;
+        for(int indexA = a.length() - 1, indexB = b.length() - 1; indexA >= 0 || indexB >= 0; indexA--, indexB--) {
+            int valA = indexA >= 0 ? a.charAt(indexA) - '0' : 0;
+            int valB = indexB >= 0 ? b.charAt(indexB) - '0' : 0;
+            int val = (valA + valB + carry) % 2;
+            carry = (valA + valB + carry) / 2;
+            val += '0';
+            sb.append((char)val);
         }
 
-        if (addOne) {
+        if (carry > 0) {
             sb.append('1');
         }
         return sb.reverse().toString();
-    }
-
-    private char getaChar(String a, int indexA) {
-        if (indexA < 0) {
-            return '0';
-        }
-        return a.charAt(indexA);
     }
 }
